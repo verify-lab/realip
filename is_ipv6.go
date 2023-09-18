54f7603c0b106c3102f949155a79669ada1f3a93
Roman Zhuravel <roman.zhuravel@theopentag.com>
2023-09-18 14:39:56 +0300

    init

--- is_ipv6.go
+++ is_ipv6.go
@@ -1,17 +1,14 @@
 package realip
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 func IsIPv6(ip net.IP) bool {
 	return IsStringIPv6(ip.String())
 }
 
 func IsStringIPv6(ip string) bool {
-	for i := 0; i < len(ip); i++ {
-		if ':' == ip[i] {
-			return true
-		}
-	}
-
-	return false
+	return strings.Contains(ip, ":")
 }
